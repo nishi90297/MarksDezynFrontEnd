fb3c0975b364ed556ca71c2e9bb2a9f641dfc2ad
Shivendra Singh Tanwar <sunnysinghtanwar2@gmail.com>
2020-03-02 01:21:48 +0530

    Add form names for updating form details

---
+++ src/app/Models/FurnitureOptionDetails.ts
@@ -0,0 +1,9 @@
+export class FurnitureOptionsDetails{
+  public name: String = '';
+  public count: Number= 0;
+  public selected: Boolean = false
+
+  constructor(name: String) {
+    this.name = name
+  }
+}

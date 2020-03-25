36f0019cfb614fb6fb38679edccf048d45c88a26
nishi90297 <nishi.90297@gmail.com>
2020-03-25 13:01:34 +0530

    pages display according to roles

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -6,18 +6,19 @@ export interface RouteInfo {
     title: string;
     icon: string;
     class: string;
+    role: string;
 }
 
 export const ROUTES: RouteInfo[] = [
-    { path: '/dashboard',     title: 'Dashboard',         icon:'nc-bank',       class: '' },
-    { path: '/dashboard/allClientsList',     title: 'All Client List',         icon:'nc-bank',       class: '' },
-    { path: '/dashboard/meetings',     title: 'Meetings | Pre-sales',         icon:'nc-bell-55',       class: '' },
-    { path: '/dashboard/designerMeetings',     title: 'Meetings | Designer',         icon:'nc-bell-55',       class: '' },
-    { path: '/dashboard/designerClientMet',     title: 'Client Met | Designer',         icon:'nc-bell-55',       class: '' },
-    { path: '/dashboard/addClient',         title: 'Add Client',             icon:'nc-diamond',    class: '' },
-    { path: '/dashboard/designQuotation', title: 'Design Quotation',     icon:'nc-bell-55',    class: '' },
-    { path: '/dashboard/requirementForm',          title: 'Requirement Form',      icon:'nc-tile-56',  class: '' },
-    { path: '/dashboard/user',         title: 'User Profile',        icon:'nc-single-02',    class: '' },
+    { path: '/dashboard',     title: 'Dashboard',         icon:'nc-bank',       class: '' , role:"ROLE_PRESALES"},
+    // { path: '/dashboard/allClientsList',     title: 'All Client List',         icon:'nc-bank',       class: '' , role:[]},
+    { path: '/dashboard/meetings',     title: 'Meetings | Pre-sales',         icon:'nc-bell-55',       class: '' , role:"ROLE_PRESALES"},
+    { path: '/dashboard/designerMeetings',     title: 'Meetings | Designer',         icon:'nc-bell-55',       class: '' , role:"ROLE_DESIGNER" },
+    { path: '/dashboard/designerClientMet',     title: 'Client Met | Designer',         icon:'nc-bell-55',       class: '', role:"ROLE_DESIGNER" },
+    // { path: '/dashboard/addClient',         title: 'Add Client',             icon:'nc-diamond',    class: '' , role:[]},
+    // { path: '/dashboard/designQuotation', title: 'Design Quotation',     icon:'nc-bell-55',    class: '' , role:[]},
+    // { path: '/dashboard/requirementForm',          title: 'Requirement Form',      icon:'nc-tile-56',  class: '' , role:[]},
+    // { path: '/dashboard/user',         title: 'User Profile',        icon:'nc-single-02',    class: '', role:[] },
 ];
 
 @Component({
@@ -29,6 +30,18 @@ export const ROUTES: RouteInfo[] = [
 export class SidebarComponent implements OnInit {
     public menuItems: any[];
     ngOnInit() {
-        this.menuItems = ROUTES.filter(menuItem => menuItem);
+        if(localStorage.getItem("role")=="ROLE_PRESALES"){
+            this.menuItems = ROUTES.filter(menuItem => menuItem.role==="ROLE_PRESALES");
+        }
+        else if(localStorage.getItem("role")=="ROLE_DESIGNER"){
+            this.menuItems = ROUTES.filter(menuItem => menuItem.role==="ROLE_DESIGNER");
+        }
+        else if(localStorage.getItem("role")=="ROLE_TL"){
+            this.menuItems = ROUTES.filter(menuItem => menuItem.role==="ROLE_TL");
+        }
+        else if(localStorage.getItem("role")=="ROLE_MANAGER"){
+            this.menuItems = ROUTES.filter(menuItem => menuItem.role==="ROLE_MANAGER");
+        }
+        
     }
 }

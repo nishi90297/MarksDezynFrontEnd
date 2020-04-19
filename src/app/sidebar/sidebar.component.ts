ef7a33f9e31c92469f1ebaece73d017ca0284b36
nishi90297 <nishi.90297@gmail.com>
2020-04-20 00:49:18 +0530

    sidebar updated according to roles

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -45,11 +45,9 @@ export class SidebarComponent implements OnInit {
     public menuItems: any[];
     ngOnInit() {
         if (localStorage.getItem('role') === 'ROLE_PRESALES') {
-            // this.menuItems = ROUTES.filter(menuItem => menuItem.role==="ROLE_PRESALES");
-            this.menuItems = ROUTES
+            this.menuItems = ROUTES.filter(menuItem => menuItem.role==="ROLE_PRESALES");
         } else if (localStorage.getItem('role') === 'ROLE_DESIGNER') {
-            // this.menuItems = ROUTES.filter(menuItem => menuItem.role==="ROLE_DESIGNER");
-            this.menuItems = ROUTES
+            this.menuItems = ROUTES.filter(menuItem => menuItem.role==="ROLE_DESIGNER");
         } else if (localStorage.getItem('role') === 'ROLE_TL') {
             this.menuItems = ROUTES.filter(menuItem => menuItem.role === 'ROLE_TL');
         } else if (localStorage.getItem('role') === 'ROLE_MANAGER') {

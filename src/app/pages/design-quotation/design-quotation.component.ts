e9a5d7c831b07d187b8e225972611de9844f40d9
Shivendra Singh Tanwar <sunnysinghtanwar2@gmail.com>
2020-04-22 11:48:25 +0530

    changes in design quotation

--- src/app/pages/design-quotation/design-quotation.component.ts
+++ src/app/pages/design-quotation/design-quotation.component.ts
@@ -12,81 +12,81 @@ import { DesignQuotation } from 'app/Models/DesignQuotation';
   styleUrls: ['./design-quotation.component.scss'],
   providers: [MessageService]
 })
-export class DesignQuotationComponent implements OnInit{
+export class DesignQuotationComponent implements OnInit {
 
-  design:Design[]=[
-    {roomName:"BEDROOM",roomType:"bedRoom",count:0},
-    {roomName:"BATHROOM",roomType:"kitchen",count:0},
-    {roomName:"KITCHEN",roomType:"bathRoom",count:0},
-    {roomName:"LIVING ROOM",roomType:"livingRoom",count:0}
+  design: Design[] = [
+    {roomName: 'BEDROOM', roomType: 'bedRoom', count: 0},
+    {roomName: 'BATHROOM', roomType: 'kitchen', count: 0},
+    {roomName: 'KITCHEN', roomType: 'bathRoom', count: 0},
+    {roomName: 'LIVING ROOM', roomType: 'livingRoom', count: 0}
   ];
 
-  public countRooms: Number[]=[0,1,2,3,4,5,6,7,8,9,10];
+  public countRooms: Number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   errorMsg: string;
   status: string;
 
-  designQuotation: DesignQuotation={
-    design:[],
+  designQuotation: DesignQuotation = {
+    design: [],
     view3D: 0,
-    adhocCharges:0,
-    clientId:0
+    adhocCharges: 0,
+    clientId: 0
   };
 
-  clientId:Number;
-  designQuotationResponse:DesignQuotationResponse
+  clientId: Number;
+  designQuotationResponse: DesignQuotationResponse
   url: String;
-  extraRoom:String="";
-  adhocCharges:Number;
-  view3D: Number=0;
+  extraRoom: String = '';
+  adhocCharges: Number;
+  view3D: Number = 0;
   // All error
   errorTypes = {
     internalServerError: 'Internal Server Error',
     somethingWentWrong: 'Something went wrong'
   };
   constructor(private designQuotationService: DesignQuotationServiceService,
-     private router: Router, 
-     private route:ActivatedRoute,
-     private toast: MessageService){}
-  
+     private router: Router,
+     private route: ActivatedRoute,
+     private toast: MessageService) {}
+
   ngOnInit() {
     this.route.queryParams.subscribe(params => {
-      this.clientId=params.id;
+      this.clientId = params.id;
   })
   }
 
-  addRoom(){
-    if(this.extraRoom.length==0){
-      this.errorPopUp(this.errorTypes.internalServerError,"Please Select a Room Name !");
-    } else if(this.design.filter(obj=>{return obj.roomName===this.extraRoom}).length!==0){
-      this.errorPopUp(this.errorTypes.internalServerError,"Room Already Exists !");
-    } else{
-      this.design.push({roomName:this.extraRoom,roomType:"customRoom",count:0})
+  addRoom() {
+    if (this.extraRoom.length === 0) {
+      this.errorPopUp(this.errorTypes.internalServerError, 'Please Select a Room Name !');
+    } else if (this.design.filter(obj => obj.roomName === this.extraRoom).length !== 0) {
+      this.errorPopUp(this.errorTypes.internalServerError, 'Room Already Exists !');
+    } else {
+      this.design.push({roomName: this.extraRoom, roomType: 'customRoom', count: 0})
       this.toast.add({severity: 'success', summary: 'Success', detail: 'Room Successfully Added !'});
-      this.extraRoom="";
+      this.extraRoom = '';
     }
   }
 
-  submitDesignQuotationForm(form: NgForm){
+  submitDesignQuotationForm(form: NgForm) {
 
-    this.design.map(obj=>obj.count=Number(obj.count));
-    this.designQuotation.design=this.design;
-    this.designQuotation.view3D= Number(this.view3D);
-    this.designQuotation.adhocCharges=this.adhocCharges;
-    this.designQuotation.clientId= Number(this.clientId); 
-    
-    console.log("response",this.designQuotation)
+    this.design.map(obj => obj.count = Number(obj.count));
+    this.designQuotation.design = this.design;
+    this.designQuotation.view3D = Number(this.view3D);
+    this.designQuotation.adhocCharges = this.adhocCharges;
+    this.designQuotation.clientId = Number(this.clientId);
+
+    console.log('response', this.designQuotation)
     this.designQuotationService.generateDesignQuotationForm(this.designQuotation).subscribe(
-      response => { this.designQuotationResponse=response;
+      response => { this.designQuotationResponse = response;
         this.toast.add({severity: 'success', summary: 'Success', detail: 'Design Quotation Generated !'});
-        this.router.navigate(['/dashboard/profile'+'?id='+this.clientId])
+        this.router.navigate(['/dashboard/profile' + '?id=' + this.clientId])
       },
       error => {
-        if(error.error.success==false){
+        if (error.error.success == false) {
           this.errorPopUp(this.errorTypes.internalServerError, error.error.msg);
           // this.router.navigate(['/dashboard/designerClientMet'])
         } else {
-          console.log("error", error)
-          console.log("error", error.error.errors[0].msg)
+          console.log('error', error)
+          console.log('error', error.error.errors[0].msg)
           this.errorPopUp(this.errorTypes.internalServerError, error.error.errors[0].msg);
         }
       }

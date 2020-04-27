a73945949aef1dcdd6a46f7b2c41927c187191d5
nishi90297 <nishi.90297@gmail.com>
2020-04-27 14:54:50 +0530

    presales api added

---
+++ src/app/Services/admin-presales.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpClientModule, HttpClient, HttpHeaders } from '@angular/common/http';
+import {environment} from '../../environments/environment';
+import { AllPresalesData } from 'app/Models/AllPresalesData';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminPresalesService {
+  
+  env = environment;
+  constructor(private http: HttpClient) { }
+
+  getAllPresales(){
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json',
+        'Authorization': localStorage.getItem('token')
+      }),
+    };
+
+    return this.http.get(this.env.backendURL + '/v1/admin/admin-all-presales', httpOptions)
+      .map(response => {
+        return response as AllPresalesApiResponse;
+      });
+    
+  }
+}
+
+export interface AllPresalesApiResponse {
+  success: boolean,
+  data: AllPresalesData[];
+}
--- src/app/pages/admin-presales/admin-presales.component.ts
+++ src/app/pages/admin-presales/admin-presales.component.ts
@@ -1,15 +1,93 @@
 import { Component, OnInit } from '@angular/core';
+import { FilterUtils, MessageService } from 'primeng/api';
+import { AdminPresalesService } from 'app/Services/admin-presales.service';
 
 @Component({
   selector: 'app-admin-presales',
   templateUrl: './admin-presales.component.html',
-  styleUrls: ['./admin-presales.component.scss']
+  styleUrls: ['./admin-presales.component.scss'],
+  providers: [MessageService]
 })
 export class AdminPresalesComponent implements OnInit {
 
-  constructor() { }
+  cols: any[];
+
+  presalesOptions = {
+    cols: [],
+    values: [],
+    columnSize: 1,
+    rows: 15
+  };
+
+  // All error
+  errorTypes = {
+    internalServerError: 'Internal Server Error',
+    somethingWentWrong: 'Something went wrong'
+  };
+  constructor(private adminPresalesService:AdminPresalesService,
+    private toast: MessageService) { }
 
   ngOnInit() {
+    this.setAllPresalesOptions();
+    this.getAllPresales();
+  
+    FilterUtils['custom'] = (value, filter): boolean => {
+      if (filter === undefined || filter === null || filter.trim() === '') {
+        return true;
+      }
+
+      if (value === undefined || value === null) {
+        return false;
+      }
+
+      return parseInt(filter) > value;
+    }
   }
 
+  getAllPresales() {
+    this.adminPresalesService.getAllPresales().subscribe(response => {
+      if (response.success) {
+        console.log('All Presales Data -->', response);
+        this.presalesOptions.values = response.data
+        console.log(this.presalesOptions);
+      }
+    }, error => {
+      this.errorPopUp(this.errorTypes.internalServerError, error.message);
+    });
+  }
+
+  setAllPresalesOptions() {
+    this.presalesOptions.cols = [
+      { field: 'meeting_datetime', header: 'DOM' },
+      { field: 'id', header: 'ID' },
+      { field: 'name', header: 'Name' },
+      { field: 'mobile', header: 'Contact' },
+      { field: 'city', header: 'City' },
+      { field: 'scope_of_work', header: 'Scope' },
+      { field: 'status', header: 'Status' },
+      { field: 'presales', header: 'PreSales' }
+    ];
+  }
+
+  errorPopUp(type, message) {
+    this.toast.add({
+      severity: 'error',
+      summary: type,
+      detail: message,
+      closable: true,
+      sticky: false,
+      life: 4000
+    });
+  }
+
+  infoPopUp(type, message) {
+    this.toast.add({
+      severity: 'info',
+      summary: 'INFO',
+      detail: message,
+      closable: true,
+      sticky: false,
+      life: 4000
+    });
+  }
 }
